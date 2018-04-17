e2fdfe1aa718a6f41a91d0beef3b8f016846a50b
rider7 <brandon.rider@gmail.com>
2018-04-17 18:03:21 -0500

    almost done with server

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+// requires express
+
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+  if (err){
+    console.log('Could NOT connect to database: ', err);
+  } else {
+    console.log('Connected to database: ' + config.db);
+  }
+});
+// requires mongoose
 
 app.get('*', (req, res) => {
   res.send('Server is running!');
